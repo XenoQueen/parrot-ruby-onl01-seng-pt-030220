40ae7989bffdfc43641c2058ef48e0ec9ce676ed
Maika Kailani <queenmab051@gmail.com>
2020-04-06 16:49:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,16 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
+<<<<<<< HEAD
 def parrot(string = "Squawk!")
   puts "#{string}"
   "#{string}"
 end
+=======
+def parrot("Pretty bird!")
+  puts "Squawk!"
+  return "Squawk!"
+end
+
+parrot
+>>>>>>> 69957fbe090b244a16f80d6649aa23ad6e8a624f
